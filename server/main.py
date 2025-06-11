1f1afce89e751e4b00044adeffcdc692a5e2e5d4
Alan Chaban <chpokchip@gmail.com>
2025-06-11 18:07:33 +0300

    create api endpoints

--- server/main.py
+++ server/main.py
@@ -2,4 +2,26 @@ from fastapi import FastAPI
 from server.api.crud_router import router as items_router
 
 app = FastAPI()
-app.include_router(items_router)  # Подключаем роутер
+
+# Подключаем роутер
+app.include_router(items_router)  
+
+# Главная страница
+app.get("/")
+async def root():
+    return {"message":"main page"}
+
+# О проекте
+app.get("/about")
+async def about():
+    return {"message":"about project"}
+
+# Данные
+app.get("/date")
+async def date():
+    return {"message":"date info"}
+
+# Админка
+app.get("/admin")
+async def admin():
+    return {"message":"admin info"}
